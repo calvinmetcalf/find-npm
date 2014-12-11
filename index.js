35edcb5dbd2be651ae0784f4cd78ada1123908a5
Calvin Metcalf <cmetcalf@appgeo.com>
2014-12-11 09:36:16 -0500

    deps

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-var fs = require('fs');
-var through2 = require("through2");
-var split2 = require("split2");
 var n = require("./n.json"), p = require("./p.json"), m=require("./m.json");
 var noms = require('noms');
 var wordCache = '';
@@ -10,24 +7,18 @@ var nl = n.length;
 var pl = p.length, ml = m.length;
 pi = 0;
 mi = 0;
-noms(function (next) {
-  this.push(n[ni] + ' ' + p[pi] + ' ' + m[mi]);
-  if (++mi === ml) {
-    mi = 0;
-    if (++pi === pl) {
-      pi = 0;
-      if (++ni === nl) {
-        this.push(null);
+module.exports = function () {
+  return noms(function (next) {
+    this.push(n[ni] + ' ' + p[pi] + ' ' + m[mi]);
+    if (++mi === ml) {
+      mi = 0;
+      if (++pi === pl) {
+        pi = 0;
+        if (++ni === nl) {
+          this.push(null);
+        }
       }
     }
-  }
-  next();
-}).pipe(through2(function (chunk, _, next) {
-  this.push(chunk);
-  console.log(chunk.toString());
-  this.push('\n');
-  next();
-}, function (done) {
-  done();
-  console.log('done');
-})).pipe(fs.createWriteStream('./npm.txt'));
+    next();
+  });
+};
